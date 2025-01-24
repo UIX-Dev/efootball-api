ca44ed0964a6387681aff5770272fa2ef28d24e2
chungh <chungh@unich.com>
2025-01-24 13:25:32 +0700

    add script

--- server.js
+++ server.js
@@ -16,13 +16,7 @@ app.post("/api/get-rendered-content", async (req, res) => {
 
     try {
         const browser = await puppeteer.launch();
-
         const page = await browser.newPage();
-
-        // await page.setUserAgent(
-        //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        // );
-
         await page.goto(url, { waitUntil: "networkidle0" });
 
         const content = await page.$eval("body", (el) => el.innerHTML);
